6adfb16b7a16ad3851ac23825178c0847cd123b8
Marvis Ighedosa <dosamarvis@gmail.com>
2023-12-11 05:44:43 +0100

    Initial Commit

---
+++ MarvMap/MarvMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvMapApp.swift
+//  MarvMap
+//
+//  Created by Marvis Ighedosa on 11/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MarvMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
